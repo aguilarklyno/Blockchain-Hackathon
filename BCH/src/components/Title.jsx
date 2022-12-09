b4cbeb8ceebf9417ab5022a7e4917bae51cbfe23
aguilarklyno <aguilarklyno@gmail.com>
2022-12-09 12:52:49 +0900

    シャレおつ近未来背景どどん

--- BCH/src/components/Title.jsx
+++ BCH/src/components/Title.jsx
@@ -14,10 +14,10 @@ const Title = () => {
           <h1 className='lg:text-8xl md:text-left text-5xl text-center font-bold md:my-4 my-0  '>
           <span className='lg:text-9xl text-6xl text-blue-400'>E</span>VE<span className='lg:text-9xl text-6xl text-blue-400'>N</span>.
           </h1>
-          <h1 className='lg:text-5xl md:text-left text-3xl text-center font-bold md:my-4 my-8  '>
-            The thing, which will take you to the 
-            <span className='font-bold'><span className='lg:text-6xl text-4xl'> E</span>VE<span className='lg:text-6xl text-4xl text-blue-400'>N </span></span>
-            world. 
+          <h1 className='lg:text-4xl md:text-left text-2xl text-center font-bold md:my-4 my-8  '>
+            Be even with
+            <span className='font-bold'><span className='lg:text-5xl text-3xl'> E</span>VE<span className='lg:text-5xl text-3xl text-blue-400'>N. </span></span>
+            
             {/* EVE<span className='lg:text-6xl text-4xl text-blue-400'>N</span> */}
           </h1>
 
